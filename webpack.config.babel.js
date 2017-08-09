c5b950f5a3417ea9e97c8f19a5bf47a779cc408f
Nicolaas <github@sunnysideup.co.nz>
2017-08-10 08:01:16 +1200

    PATCH: fixes

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,7 +4,7 @@
 */
 
 /*
-  Imports
+    Imports
 */
 
 import webpack from 'webpack';
@@ -15,192 +15,193 @@ import NotifierPlugin from 'webpack-notifier';
 
 
 /*
-  Useful constants
+    Useful constants
 */
 
 const SITE_NAME = path.basename(path.join(__dirname, '/../../'));
 const THEME_NAME = path.basename(__dirname);
 
 /*
-  Plugin configuration
+    Plugin configuration
 */
 
 //different css points
 const extractEditor = new ExtractTextPlugin({
-  filename: 'css/editor.css',
+    filename: 'css/editor.css',
 });
 const extractMain = new ExtractTextPlugin({
-  filename: 'css/style.css',
+    filename: 'css/style.css',
 });
 
 //define plugins
 let plugins = [];
 
 if(process.env.NODE_ENV === 'production') {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin(),
-    extractEditor,
-    extractMain
-  );
-
-
-//development
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        extractEditor,
+        extractMain
+    );
 } else {
-  plugins.push(
-    //error notifications on computer
-    new NotifierPlugin({alwaysNotify: true}),
-    //auto updating on dev server
-    new webpack.HotModuleReplacementPlugin(),
-    //shows relative path in HotModuleReplacement
-    new webpack.NamedModulesPlugin(),
-    //sexy dashboard
-    new DashboardPlugin(),
-    extractEditor
-  );
+    plugins.push(
+        //error notifications on computer
+        new NotifierPlugin({alwaysNotify: true}),
+        //auto updating on dev server
+        new webpack.HotModuleReplacementPlugin(),
+        //shows relative path in HotModuleReplacement
+        new webpack.NamedModulesPlugin(),
+        //sexy dashboard
+        new DashboardPlugin(),
+        extractEditor
+    );
 }
 
-plugins.push(new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery",
-    "window.jQuery": "jquery"
-}))
+plugins.push(
+    new webpack.ProvidePlugin(
+        {
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }
+    )
+)
 
 const sources = ["../sswebpack_base/src", '../sswebpack_mysite/src'];
 
 const sassFolders = sources.map((source) => path.resolve(source, "scss"))
-  .concat(sources.map((source) => path.resolve(source, "sass")));
+    .concat(sources.map((source) => path.resolve(source, "sass")));
 
 //HMR can be fixed by using basic loaders instead of textExtract
 const sassLoader = {
-  fallback: 'style-loader',
-  use: [
-    'css-loader',
-    {
-      loader: 'postcss-loader',
-      options: {
-        sourceMap: true
-      }
-    },
-    {
-      loader: 'sass-loader',
-      options: {
-        sourceMap: true
-      }
-    },
-    'import-glob-loader'
-  ]
+    fallback: 'style-loader',
+    use: [
+        'css-loader',
+        {
+            loader: 'postcss-loader',
+            options: {
+                sourceMap: true
+            }
+        },
+        {
+            loader: 'sass-loader',
+            options: {
+                sourceMap: true
+            }
+        },
+        'import-glob-loader'
+    ]
 }
 
 const styleLoaders = [{
-  //basic css
-  test: /\.css/i,
-  use: ['style-loader', 'css-loader']
+    //basic css
+    test: /\.css/i,
+    use: ['style-loader', 'css-loader']
 }, {
-  //main styles
-  test: /[^editor].\.s(a|c)ss$/i,
-  include: sassFolders,
-  use: extractMain.extract(sassLoader)
+    //main styles
+    test: /[^editor].\.s(a|c)ss$/i,
+    include: sassFolders,
+    use: extractMain.extract(sassLoader)
 }, {
-  //styles for editor
-  test: /editor\.s(a|c)ss/i,
-  include: sassFolders,
-  use: extractEditor.extract(sassLoader)
+    //styles for editor
+    test: /editor\.s(a|c)ss/i,
+    include: sassFolders,
+    use: extractEditor.extract(sassLoader)
 }];
 
 const jsLoaders = [{
-  //eslint check
-  enforce: 'pre',
-  test: /\.js$/i,
-  exclude: /node_modules/,
-  use: {
-    loader: 'eslint-loader'
-  }
-}, {
-  //js compilation
-  test: /\.js$/i,
-  include: sources.map((source) => path.resolve(source, "src")),
-  exclude: /node_modules/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      cacheDirectory: true,
-      presets: [require.resolve("babel-preset-es2015")]
+    //eslint check
+    enforce: 'pre',
+    test: /\.js$/i,
+    exclude: /node_modules/,
+    use: {
+        loader: 'eslint-loader'
     }
-  }
+}, {
+    //js compilation
+    test: /\.js$/i,
+    include: sources.map((source) => path.resolve(source, "src")),
+    exclude: /node_modules/,
+    use: {
+        loader: 'babel-loader',
+        options: {
+            cacheDirectory: true,
+            presets: [require.resolve("babel-preset-es2015")]
+        }
+    }
 }];
 
 const imageLoaders = [{
-  test: /\.(png|jpg|gif)$/i,
-  include: sources.map((source) => path.resolve(source, "images")),
-  use: [
-    {
-      loader: 'url-loader',
-      options: {
-        limit: 30000,
-        name: 'images/[name].[ext]'
-      }
-    },
-    {
-      loader: 'image-webpack-loader',
-      options: {
-        optipng: {
-          optimizationLevel: 5
+    test: /\.(png|jpg|gif)$/i,
+    include: sources.map((source) => path.resolve(source, "images")),
+    use: [
+        {
+            loader: 'url-loader',
+            options: {
+                limit: 30000,
+                name: 'images/[name].[ext]'
+            }
         },
-        mozjpeg: {
-          interlaced: true,
+        {
+            loader: 'image-webpack-loader',
+            options: {
+                optipng: {
+                    optimizationLevel: 5
+                },
+                mozjpeg: {
+                    interlaced: true,
+                }
+            }
         }
-      }
-    }
-  ]
+    ]
 },
 {
-  test: /\.svg$/i,
-  use: 'svg-inline-loader'
+    test: /\.svg$/i,
+    use: 'svg-inline-loader'
 }];
 
 
 /*
-  Main Config Object
+    Main Config Object
 */
 export default {
-  //what files to start from
-  //bundle should include main.js from all sources
-  entry: sources.map((source) => path.resolve(source, "main.js")),
-  //access from client
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: `/themes/${THEME_NAME}/dist/`,
-    filename: 'bundle.js'
-  },
-  //loaders
-  module: {
-    rules: styleLoaders.concat(jsLoaders).concat(imageLoaders)
-  },
-  //extra settings
-  resolve: {
+    //what files to start from
+    //bundle should include main.js from all sources
+    entry: sources.map((source) => path.resolve(source, "main.js")),
+    //access from client
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: `/themes/${THEME_NAME}/dist/`,
+        filename: 'bundle.js'
+    },
+    //loaders
+    module: {
+        rules: styleLoaders.concat(jsLoaders).concat(imageLoaders)
+    },
+    //extra settings
+    resolve: {
         modules: [
             path.join(__dirname, "node_modules"),
         ],
         extensions: [".js", ".jsx"]
     },
-  devServer: {
-    disableHostCheck: true,
-    host: '0.0.0.0',
-    hot: true,
-    port: 3000,
-    publicPath: `/themes/${THEME_NAME}/dist/`,
-    proxy: {
-      '/': {
-        'target': {
-          'host': `${SITE_NAME}.localhost`,
-          'protocol': 'http',
-          'port': 80
+    devServer: {
+        disableHostCheck: true,
+        host: '0.0.0.0',
+        hot: true,
+        port: 3000,
+        publicPath: `/themes/${THEME_NAME}/dist/`,
+        proxy: {
+            '/': {
+                'target': {
+                    'host': `${SITE_NAME}.localhost`,
+                    'protocol': 'http',
+                    'port': 80
+                },
+                changeOrigin: true,
+                secure: false
+            }
         },
-        changeOrigin: true,
-        secure: false
-      }
+        stats: 'errors-only'
     },
-    stats: 'errors-only'
-  },
-  plugins: plugins,
+    plugins: plugins,
 };
